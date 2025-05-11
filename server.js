16477b0409a03fd8f4fbb917933ef5e54a0c89cd
luizfilipeyep <83364189+luizfilipeyep@users.noreply.github.com>
2025-05-11 11:51:00 -0300

    connect to neon db

--- server.js
+++ server.js
@@ -8,13 +8,20 @@ app.use(cors())
 app.use(express.json())
 
 // Configurar pool de conexão com PostgreSQL
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+// })
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 // Rota para listar usuários
 app.get("/users", async (req, res) => {  
